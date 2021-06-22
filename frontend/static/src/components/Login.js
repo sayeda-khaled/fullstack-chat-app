327ca31f66932bde67dd7f6370ac87a01f70c017
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-06-22 19:09:47 -0400

    styled the login and register forms

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -27,12 +27,24 @@ class Login extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" name="username" onChange={this.handleInput} value={this.state.username}/>
-        <input type="email" name="email" onChange ={this.handleInput} value={this.state.email}/>
-        <input type="password" name="password" onChange={this.handleInput} value={this.state.password1}/>
-        <button type="submit">Login</button>
+        <div class="form-group">
+          <label for="formGroupExampleInput2">User Name</label>
+          <input type="text" class="form-control" name="username" onChange={this.handleInput} value={this.state.username} id="formGroupExampleInput2" placeholder="User Name" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" name="email" onChange={this.handleInput} value={this.state.email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" name="password" onChange={this.handleInput} value={this.state.password} id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
       </form>
-    );
+  );
   }
 }
+
+
+
 export default Login;
--- frontend/static/src/components/Registration.js
+++ frontend/static/src/components/Registration.js
@@ -27,12 +27,25 @@ class Registration extends Component {
 
   render() {
     return (
+
       <form onSubmit={this.handleSubmit}>
-        <input type="text" name="username" onChange={this.handleInput} value={this.state.username} required />
-        <input type="email" name="email" required onChange ={this.handleInput} value={this.state.email}/>
-        <input type="password" name="password1" required onChange={this.handleInput} value={this.state.password1}/>
-        <input type="password" name="password2" required onChange={this.handleInput} value={this.state.password2}/>
-        <button type="submit">Register</button>
+        <div class="form-group">
+          <label for="formGroupExampleInput2">User Name</label>
+          <input type="text" class="form-control" name="username" required onChange={this.handleInput} value={this.state.username} id="formGroupExampleInput2" placeholder="User Name" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" name="email" required onChange={this.handleInput} value={this.state.email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" name="password1" required onChange={this.handleInput} value={this.state.password1} id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Confirm Password</label>
+          <input type="password" class="form-control" name="password2" required onChange={this.handleInput} value={this.state.password2} id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
       </form>
     );
   }
