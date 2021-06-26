5fa82a9784be51c52ac8903027c68c9fd836b60b
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-06-26 18:07:38 -0400

    added the register button to the registration form

--- frontend/static/src/components/Registration.js
+++ frontend/static/src/components/Registration.js
@@ -34,21 +34,23 @@ class Registration extends Component {
       <form onSubmit={this.handleSubmit}>
         <div class="form-group">
           <label for="formGroupExampleInput2">User Name</label>
-          <input type="text" class="form-control" name="username" required onChange={this.handleInput} value={this.state.username} id="formGroupExampleInput2" placeholder="User Name" />
+          <input type="text" class="form-control" name="username" autoComplete="off" required onChange={this.handleInput} value={this.state.username} id="formGroupExampleInput2" placeholder="User Name" />
         </div>
         <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" name="email" required onChange={this.handleInput} value={this.state.email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <input type="email" class="form-control" name="email" autoComplete="off" required onChange={this.handleInput} value={this.state.email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" name="password1" required onChange={this.handleInput} value={this.state.password1} id="exampleInputPassword1" placeholder="Password" />
+          <input type="password" class="form-control" name="password1" autoComplete="off" required onChange={this.handleInput} value={this.state.password1} id="exampleInputPassword1" placeholder="Password" />
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Confirm Password</label>
-          <input type="password" class="form-control" name="password2" required onChange={this.handleInput} value={this.state.password2} id="exampleInputPassword1" placeholder="Password" />
+          <input type="password" class="form-control" name="password2" autoComplete="off" required onChange={this.handleInput} value={this.state.password2} id="exampleInputPassword1" placeholder="Password" />
         </div>
-      </form>      
+        <button type="submit" class="btn btn-secondary offset" onClick={() => this.props.handleSelection('registration')}>Register</button>
+      </form>
+
     );
   }
 }
